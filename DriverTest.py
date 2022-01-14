8f3dcab704b79de59aa7f4a53fa2fdc70cde9c12
nuovonatura <nuovonatura@outlook.com>
2022-01-14 12:02:41 +0800

    Remove redundant file.

--- DriverTest.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
-opts = Options()
-opts.set_headless(True)
-assert opts.headless
-browser = Firefox(options=opts)
-browser.get('https://baidu.com')
